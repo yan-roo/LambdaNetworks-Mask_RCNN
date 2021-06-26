874720749aef982a952782ff159681d9e732ada6
yan-roo <49684416+yan-roo@users.noreply.github.com>
2021-06-26 15:03:40 +0800

    Update LambdaResNet50 config
    * Add SyncBN to all heads
    
    * Add RandomCrop to fixed 1024

--- configs/lambda_resnet/mask_rcnn_lambda_resnet_50_1024.py
+++ configs/lambda_resnet/mask_rcnn_lambda_resnet_50_1024.py
@@ -1,20 +1,24 @@
 # model settings
+norm_cfg = dict(type='SyncBN', momentum=0.01, eps=1e-3, requires_grad=True)
 model = dict(
     type='MaskRCNN',
     backbone=dict(
         type='LambdaResNet',
+        pretrained=None,
         depth=50,
         num_stages=4,
         out_indices=(0, 1, 2, 3),
-        frozen_stages=1,
-        norm_cfg=dict(type='SyncBN', requires_grad=True),
+        frozen_stages=1, 
+        zero_init_residual=True,
+        norm_cfg=norm_cfg,
         norm_eval=True,
         style='pytorch'),
     neck=dict(
         type='FPN',
         in_channels=[256, 512, 1024, 2048],
         out_channels=256,
-        num_outs=5),
+        num_outs=5,
+        norm_cfg=norm_cfg),
     rpn_head=dict(
         type='RPNHead',
         in_channels=256,
@@ -39,9 +43,11 @@ model = dict(
             out_channels=256,
             featmap_strides=[4, 8, 16, 32]),
         bbox_head=dict(
-            type='Shared2FCBBoxHead',
+            type='Shared4Conv1FCBBoxHead',
             in_channels=256,
             fc_out_channels=1024,
+            conv_out_channels=256,
+            norm_cfg=norm_cfg,
             roi_feat_size=7,
             num_classes=80,
             bbox_coder=dict(
@@ -62,6 +68,7 @@ model = dict(
             num_convs=4,
             in_channels=256,
             conv_out_channels=256,
+            norm_cfg=norm_cfg,
             num_classes=80,
             loss_mask=dict(
                 type='CrossEntropyLoss', use_mask=True, loss_weight=1.0))),
@@ -120,13 +127,14 @@ model = dict(
             
 # dataset settings
 dataset_type = 'CocoDataset'
-data_root = '/data/yanroo/data/coco/'
+data_root = './data/coco/'
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 train_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(type='LoadAnnotations', with_bbox=True, with_mask=True),
     dict(type='Resize', img_scale=(1024, 1024), ratio_range=(0.1, 2.0), keep_ratio=True),
+    dict(type='RandomCrop', crop_size=(1024, 1024)),
     dict(type='RandomFlip', flip_ratio=0.5),
     dict(type='Normalize', **img_norm_cfg),
     dict(type='Pad', size_divisor=32),
@@ -149,8 +157,8 @@ test_pipeline = [
         ])
 ]
 data = dict(
-    samples_per_gpu=2,
-    workers_per_gpu=2,
+    samples_per_gpu=6,
+    workers_per_gpu=4,
     train=dict(
         type=dataset_type,
         ann_file=data_root + 'annotations/instances_train2017.json',
@@ -169,13 +177,13 @@ data = dict(
 evaluation = dict(metric=['bbox', 'segm'])
 
 # optimizer
-optimizer = dict(type='SGD', lr=0.32, momentum=0.9, weight_decay=4e-5) # batch_size 256
+optimizer = dict(type='SGD', lr=0.06, momentum=0.9, weight_decay=4e-5) # batch_size 256
 optimizer_config = dict(grad_clip=None)
 # learning policy
 lr_config = dict(
     policy='step',
     warmup='linear',
-    warmup_iters=1000,
+    warmup_iters=5333,
     warmup_ratio=0.1,
     step=[252, 266, 273])
 runner = dict(type='EpochBasedRunner', max_epochs=280)
@@ -186,7 +194,7 @@ log_config = dict(
     interval=50,
     hooks=[
         dict(type='TextLoggerHook'),
-        # dict(type='TensorboardLoggerHook')
+        dict(type='TensorboardLoggerHook')
     ])
 # yapf:enable
 custom_hooks = [dict(type='NumClassCheckHook')]
@@ -196,3 +204,4 @@ log_level = 'INFO'
 load_from = None
 resume_from = None
 workflow = [('train', 1)]
+work_dir = './work_dirs/mask_rcnn_lambda_resnet_50_1024_fix_batch6_layer'
